ed4cc5e38d297867d981e4a6f787852e1be34203
Marcus <marcussoh38@gmail.com>
2025-11-09 22:18:07 +0800

    chore(api): transfer

--- api/src/utils/notifications.ts
+++ api/src/utils/notifications.ts
@@ -6,11 +6,11 @@ interface IClaimMapEntry {
 
 const ClaimMap: {
   [machineId: string]: IClaimMapEntry[];
-} = {}
+} = {};
 
 const UserMap: {
   [fcmToken: string]: string; // machineId
-} = {}
+} = {};
 
 const ClaimedUsers = new Set<string>();
 
@@ -41,28 +41,29 @@ export const unclaimMachine = (machineId: string, fcmToken: string) => {
   }
 
   // ClaimMap[machineId] = ClaimMap[machineId].filter(claim => claim.fcmToken !== fcmToken);
-  ClaimMap[machineId] = []
+  ClaimMap[machineId] = [];
   delete UserMap[fcmToken];
   // ClaimedUsers.delete(fcmToken);
 
-
   console.log("Current ClaimMap:", ClaimMap);
   console.log("Current UserMap:", UserMap);
-
-}
-
+};
 
 /**
  * Claim a machine for a user
- * 
+ *
  * Note: only one user can claim a machine at a time
  * Note: only one machine can be claimed by a user at a time
- * 
- * @param machineId 
- * @param fcmToken 
- * @param cycleTime 
+ *
+ * @param machineId
+ * @param fcmToken
+ * @param cycleTime
  */
-export const claimMachine = (machineId: string, fcmToken: string, cycleTime: number) => {
+export const claimMachine = (
+  machineId: string,
+  fcmToken: string,
+  cycleTime: number
+) => {
   const claimedAt = new Date();
   // const expirationTime = new Date(claimedAt.getTime() + cycleTime * 60000);
 
@@ -70,7 +71,7 @@ export const claimMachine = (machineId: string, fcmToken: string, cycleTime: num
     ClaimMap[machineId] = [];
   }
 
-  if (ClaimMap[machineId].some(claim => claim.fcmToken === fcmToken)) {
+  if (ClaimMap[machineId].some((claim) => claim.fcmToken === fcmToken)) {
     // already claimed by this user
     return; // no error
   }
@@ -86,6 +87,7 @@ export const claimMachine = (machineId: string, fcmToken: string, cycleTime: num
   // }
   // ClaimedUsers.add(fcmToken);
 
+  // if the user has already claimed another machine, unclaim it
   const previousMachineId = UserMap[fcmToken];
   if (previousMachineId) {
     // unclaim previous machine
@@ -102,9 +104,7 @@ export const claimMachine = (machineId: string, fcmToken: string, cycleTime: num
   // setTimeout(() => {
   //   ClaimMap[machineId] = ClaimMap[machineId].filter(claim => claim.fcmToken !== fcmToken);
   // }, expirationTime.getTime() - claimedAt.getTime());
-}
-
-
+};
 
 export const getClaimants = (machineId: string): IClaimMapEntry[] => {
   if (!ClaimMap[machineId]) {
@@ -112,4 +112,4 @@ export const getClaimants = (machineId: string): IClaimMapEntry[] => {
   }
 
   return ClaimMap[machineId];
-}
+};
