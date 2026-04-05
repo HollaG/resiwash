ca06ecffbb1ac9d537680a55880230246780ad26
Marcus <marcussoh38@gmail.com>
2026-04-05 12:23:07 +0800

    fix(api): pw incorrect

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import dotenv from "dotenv";
 import { DataSource } from "typeorm";
 import { User } from "./models/User";
 import { Area } from "./models/Area";
@@ -11,12 +12,14 @@ import { SensorToMachine } from "./models/SensorToMachine";
 import { Claim } from "./models/Claim";
 import { LastPoke } from "./models/LastPoke";
 
+dotenv.config();
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || undefined,
+  password: process.env.DB_PASSWORD ?? "",
   database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
